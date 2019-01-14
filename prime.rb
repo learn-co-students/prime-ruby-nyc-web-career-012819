3b473a1b1665421ea51c50df59b5d92082bb4a7c
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-01-14 16:05:18 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime? int
+  if int <= 1
+    return false
+  end
+  for num in 2..(int-1)
+    if (int % num) == 0
+      return false
+    end
+  end
+  true
+end
+
+
+# prime number: a positive integer that is divisible by one and itself only
