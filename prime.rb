662758c26c3418b9b1775f26a285a722184790fb
github username <github email address>
2019-01-15 15:43:36 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
-def prime?(integer)
-  if integer >= 2
-    (2..integer - 1).all? do |x|
-      integer % x != 0
+def prime?(integers)
+  if integers >= 2
+    (2..integers - 1).all? do |x|
+      integers % x != 0
     end
   else
     return false
